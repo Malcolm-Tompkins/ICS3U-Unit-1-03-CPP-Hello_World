af60b24d98ade39f53e1d9838e11378d89a67e7e
Ubuntu <ubuntu@ip-172-31-27-115.ec2.internal>
2021-04-26 18:39:22 +0000

    Added helloWorld.cpp, with proper style

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Malcolm Tompkins All rights reserved
+//
+// Created by Malcolm Tompkins
+// Created on April 26, 2021
+// This is a Hello World file with proper C++ style
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World";
+}
